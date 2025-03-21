26c077b1aeb375d6ba80a2d6511b35c8f6fd29d2
amarinc2000 <a.marinc@alumnos.upm.es>
2025-03-21 17:13:26 +0100

    creacion proyecto spring boot

---
+++ grupo14-belleza/src/main/java/es/upm/dit/isst/grupo14_belleza/Grupo14BellezaApplication.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.grupo14_belleza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo14BellezaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo14BellezaApplication.class, args);
+	}
+
+}
---
+++ grupo14-belleza/src/test/java/es/upm/dit/isst/grupo14_belleza/Grupo14BellezaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.grupo14_belleza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo14BellezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
