1dbe2eb64be055ff5c758ceb70fbca0b25b6bf6d
amarinc2000 <a.marinc@alumnos.upm.es>
2025-03-26 12:58:48 +0100

    mejoras navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import './Navbar.css'; // Opcional, si quieres añadir estilos
+/* // import './Navbar.css'; // Opcional, si quieres añadir estilos
 
 const Navbar = () => {
   return (
@@ -11,6 +11,42 @@ const Navbar = () => {
     <li><a href="/contacto">Contacto</a></li>
   </ul>
 </nav>
+    </nav>
+  );
+};
+
+export default Navbar; */
+import { Link } from "react-router-dom";
+
+
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+      <div className="container">
+        <Link className="navbar-brand" to="/">Belleza</Link>
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto"> {/* ms-auto alinea la lista a la derecha */}
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Inicio</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/servicios">Servicios</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contacto">Contacto</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
