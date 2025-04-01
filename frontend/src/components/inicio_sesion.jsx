999743dec3a10ac23d5317c5f3ade4767cd8dc12
Victor Mendizabal <v.mendizabal@alumnos.upm.es>
2025-04-01 10:14:42 +0200

    Cambios en el frontend (inicio_sesion.jsx) Modificaciones css en inicio sesion y App

--- frontend/src/components/inicio_sesion.jsx
+++ frontend/src/components/inicio_sesion.jsx
@@ -1,64 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./inicio_sesion.css"; // Importa el archivo CSS
 
 const InicioSesion = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-pink-300 via-purple-300 to-blue-300">
-      <h1 className="text-5xl font-extrabold text-white mb-10 drop-shadow-lg">BELLEZA</h1>
-
-      <div className="flex flex-col items-center p-6 rounded-lg bg-gray-50 shadow-md hover:shadow-lg transition-all duration-300">
-        {/* Sección Cliente */}
-        <div className="flex flex-col items-center">
-          <h2 className="text-lg font-bold mb-4">CLIENTE</h2>
+    <div className="inicio-sesion-container">
+      <div className="contenedor-secciones">
+        <div className="seccion">
+          <h2 className="titulo-seccion">CLIENTES</h2>
           <input
             type="email"
             placeholder="Email"
-            className="w-64 p-3 mb-3 border rounded-lg text-gray-600 italic bg-white"
+            className="input"
           />
           <input
             type="password"
             placeholder="Contraseña"
-            className="w-64 p-3 mb-3 border rounded-lg text-gray-600 italic bg-white"
+            className="input"
           />
-          <button className="w-64 p-4 mb-4 bg-purple-500 text-white font-bold rounded-lg shadow-lg hover:bg-purple-600 transition-all duration-300">
-            ACCEDER
-          </button>
-          <button className="w-64 p-4 mb-4 bg-purple-500 text-white font-bold rounded-lg shadow-lg hover:bg-purple-600 transition-all duration-300">
-            CREAR NUEVA CUENTA
-          </button>
+          <button className="boton-acceder">ACCEDER</button>
+          <button className="boton-crear-cuenta">CREAR NUEVA CUENTA</button>
         </div>
 
-        {/* Sección Negocio */}
-        <div className="flex flex-col items-center">
-          <h2 className="text-lg font-bold mb-4">NEGOCIO</h2>
+        <div className="seccion">
+          <h2 className="titulo-seccion">EMPRESAS</h2>
           <input
             type="email"
             placeholder="Email Profesional"
-            className="w-64 p-3 mb-3 border rounded-lg text-gray-600 italic bg-white focus:border-purple-500 focus:outline-none"
+            className="input"
           />
           <input
             type="password"
             placeholder="Contraseña"
-            className="w-64 p-3 mb-3 border rounded-lg text-gray-600 italic bg-white"
+            className="input"
           />
-          <button className="w-64 p-4 mb-4 bg-purple-500 text-white font-bold rounded-lg shadow-lg hover:bg-purple-600 transition-all duration-300">
-            ACCEDER
-          </button>
-          <button className="w-64 p-4 mb-4 bg-purple-500 text-white font-bold rounded-lg shadow-lg hover:bg-purple-600 transition-all duration-300">
-            CREAR CUENTA NEGOCIO
-          </button>
+          <button className="boton-acceder">ACCEDER</button>
+          <button className="boton-crear-cuenta">CREAR CUENTA EMPRESA</button>
         </div>
       </div>
 
-      
-      {/* Botón para volver a la página principal */}
       <Link to="/">
-        <button className="mt-16 w-64 p-3 bg-gray-500 text-white font-bold rounded-lg">
-          VOLVER A INICIO
-        </button>
+        <button className="boton-volver">VOLVER A INICIO</button>
       </Link>
     </div>
   );
 };
 
-export default InicioSesion;
+export default InicioSesion; 
