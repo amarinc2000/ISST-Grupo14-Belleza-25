215733ca850df5e00bfd3c93a073aabbb36a0075
Cesar Escobar <cesar.escobar.angeles@alumnos.upm.es>
2025-04-11 17:25:17 +0200

    Nueva estructura 2

---
+++ backend-grupo14-belleza/src/main/java/es/upm/dit/isst/backend_grupo14_belleza/BackendGrupo14BellezaApplication.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.backend_grupo14_belleza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGrupo14BellezaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGrupo14BellezaApplication.class, args);
+	}
+
+}
---
+++ backend-grupo14-belleza/src/test/java/es/upm/dit/isst/backend_grupo14_belleza/BackendGrupo14BellezaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.backend_grupo14_belleza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGrupo14BellezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
